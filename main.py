5f41be30bff7eefe9523692f2bcd41be61442a2d
seetharamanrs <seetharaman2632001@gmail.com>
2026-02-09 17:48:54 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+name=input("Enter the name: ")
+age=int(input("Enter the Age: "))
+years_left=60-age
+
+print("Hello!",name )
+print(f"You have {years_left} years to reach 60.")
+print("Today you are starting as Developer")
+
+
